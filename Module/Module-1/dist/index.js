8827fa435e47dfa58304043b80f6429452f4abe3
Md Omar Faruk <hmomarfaruk1454@gmail.com>
2025-09-06 19:33:14 +0600

    Next-Level developer

---
+++ Module/Module-1/dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Name = "Next Level";
+console.log(`Welcome to ${Name}`);
---
+++ Module/Module-1/src/index.ts
@@ -0,0 +1,2 @@
+const Name: string = "Next Level";
+console.log(`Welcome to ${Name}`); 
